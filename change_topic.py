cd37fd9aa82e2102e6bb1fd14e97aa5b3b7c6905
Shuyuan Yang <sxy841@case.edu>
2023-06-12 00:28:14 -0400

    apple to a directory

--- change_topic.py
+++ change_topic.py
@@ -2,6 +2,7 @@ import rosbag
 import os
 import sys
 import shutil
+from tqdm import tqdm
 
 def change_topic_name(input_bag_path):
     # Create a temporary file for storing modified data
@@ -20,6 +21,19 @@ def change_topic_name(input_bag_path):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python change_topic.py <input_bag_path>")
+        print("Usage: python change_topic.py <directory_path>")
     else:
-        change_topic_name(sys.argv[1])
+        directory_path = sys.argv[1]
+        
+        # Collect all bag files
+        bag_files = []
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                # If the file is a .bag file
+                if file.endswith(".bag"):
+                    file_path = os.path.join(root, file)
+                    bag_files.append(file_path)
+        
+        # Process all bag files with a progress bar
+        for file_path in tqdm(bag_files, desc="Processing .bag files"):
+            change_topic_name(file_path)
